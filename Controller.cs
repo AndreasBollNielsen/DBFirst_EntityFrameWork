d0872392c4f5e42c6c135a6c6022d03b4738b947
AndreasBollNielsen <64074621+AndreasBollNielsen@users.noreply.github.com>
2022-02-23 18:37:01 +0100

    Add project files.

---
+++ Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DBFirst_EntityFrameWork
+{
+    public class Controller
+    {
+        public List<FlightRoute> GetFlightRoutes()
+        {
+           //get flightroute with foregin keys from another entity
+            using (var dbcontext = new AirportEntities())
+            {
+                var query = dbcontext.FlightRoutes.Include(x=> x.MyAirport).Include(x=>x.MyAirport1).ToList();
+               
+                return query;
+            }
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirst_EntityFrameWork
+{
+    class Program
+    {
+        static Controller controller = new Controller();
+        static void Main(string[] args)
+        {
+
+            //show data from database
+            var flightroutes = controller.GetFlightRoutes();
+
+            foreach (var item in flightroutes)
+            {
+                Console.WriteLine($"flight id {item.FlightRoute_Id} From  {item.MyAirport.name} To {item.MyAirport1.name}");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
